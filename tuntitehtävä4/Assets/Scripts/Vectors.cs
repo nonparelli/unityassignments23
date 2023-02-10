b5f1de20ba5db330cf7f6073f37dcbb412d779c4
nonparelli <90031028+nonparelli@users.noreply.github.com>
2023-02-10 14:34:20 +0200

    Update Vectors.cs

--- tuntitehtävä4/Assets/Scripts/Vectors.cs
+++ tuntitehtävä4/Assets/Scripts/Vectors.cs
@@ -11,11 +11,11 @@ public class Vectors : MonoBehaviour
     public GameObject B;
     public float scalarDot;
     public float axis_length = 2f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -26,40 +26,44 @@ public class Vectors : MonoBehaviour
     void OnDrawGizmos()
     {
         Vector3 origin = Vector3.zero;
-        //Gizmos.color;
+
+        // ACQUIRE DOT PRODUCT
         Vector2 vecA = A.transform.position;
         Vector2 vecB = B.transform.position;
-        
-        // float vecAlen = vecA.magnitude;
-        // vecAlen = Mathf.Sqrt(vecA.x * vecA.x + vecA.y * vecA.y);
-        // Vector2 vecN = vecA/vecAlen;
-       Vector2 vecNA = vecA.normalized;
+
+        //float vecAlen = vecA.magnitude;
+        //vecAlen = Mathf.Sqrt(vecA.x * vecA.x + vecA.y * vecA.y);
+        //Vector2 vecN = vecA/vecAlen;
+        Vector2 vecNA = vecA.normalized;
         Vector2 vecNB = vecB.normalized;
         scalarDot = Vector2.Dot(vecNA, vecNB);
 
-        // scalarDot = Vector2.Dot(vecA, vecB);
-        DrawVector(new Vector3( -axis_length,0, 0), new Vector3( axis_length, 0, 0), Color.red);
-        DrawVector(new Vector3(0,-axis_length,0),new Vector3( 0,axis_length, 0), Color.green);
+        //DRAW X AND Y AXIS
+        DrawVector(new Vector3(-axis_length, 0, 0), new Vector3(axis_length, 0, 0), Color.red);
+        DrawVector(new Vector3(0, -axis_length, 0), new Vector3(0, axis_length, 0), Color.green);
 
+        // DRAW DISC
         Gizmos.color = Color.white;
         Handles.DrawWireDisc(origin, Vector3.forward, 1.0f);
 
         // Draw Vector A
         DrawVector(origin, A.transform.position, Color.black);
         Gizmos.color = Color.black;
+
         // draw normalized vector from a
         DrawVector(origin, vecNA, Color.blue);
-
-        // Draw Vector B
-        DrawVector(origin,B.transform.position,Color.black);
-        Gizmos.color = Color.black;
         // draw normalized vector from b
         DrawVector(origin, vecNB, Color.blue);
 
-        //Vector projection: Dot(vecN,vecB)*vecN
-        Vector2 vecProj = vecNA * scalarDot;
-        //Draw vector projection?
-        DrawVector(origin, vecProj, Color.Lerp(Color.clear, Color.magenta, 0.5f));
+        // Draw Vector B
+        DrawVector(origin, B.transform.position, Color.black);
+        Gizmos.color = Color.black;
+
+
+        // Vector projection: Dot(vecN,vecB)*vecN
+        //Vector2 vecProj = vecNA * scalarDot;
+        // Draw vector projection?
+        //DrawVector(origin, vecProj, Color.Lerp(Color.clear, Color.magenta, 0.5f));
     }
     void DrawVector(Vector3 from, Vector3 to, Color c)
     {
