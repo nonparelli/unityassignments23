27a633d1aab335514ebbb94dc44b686dd3cd0f19
nonparelli <90031028+nonparelli@users.noreply.github.com>
2023-02-24 13:39:32 +0200

    Finalized tank

---
+++ tuntitehtävä4/Assets/Scripts/MyGizmos.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGizmos : MonoBehaviour
+{
+    public static void DrawVectorLine(Vector3 from, Vector3 to, Color c)
+    {
+        Color curr = Gizmos.color;
+        Gizmos.color = c;
+        Gizmos.DrawLine(from, to);
+
+        // Compute a location from "to towards from with 30degs"
+        Vector3 loc = -(to - from);
+        loc = Vector3.ClampMagnitude(loc, 0.1f);
+        Quaternion rot30 = Quaternion.Euler(0, 0, 30);
+        Vector3 loc1 = rot30 * loc;
+        rot30 = Quaternion.Euler(0, 0, -30);
+        Vector3 loc2 = rot30 * loc;
+        Gizmos.DrawLine(to, to + loc1);
+        Gizmos.DrawLine(to, to + loc2);
+        Gizmos.color = curr;
+
+    }
+    public static void DrawVectorDir(Vector3 from, Vector3 dir, Color c)
+    {
+        Color curr = Gizmos.color;
+        Gizmos.color = c;
+        dir.Normalize();
+        Vector3 to = from + dir;
+        Gizmos.DrawLine(from, to);
+        // Compute a location from "to towards from with 30degs"
+        Vector3 loc = -(to - from);
+        loc = Vector3.ClampMagnitude(loc, 0.1f);
+        Quaternion rot30 = Quaternion.Euler(0, 0, 30);
+        Vector3 loc1 = rot30 * loc;
+        rot30 = Quaternion.Euler(0, 0, -30);
+        Vector3 loc2 = rot30 * loc;
+        Gizmos.DrawLine(to, to + loc1);
+        Gizmos.DrawLine(to, to + loc2);
+        Gizmos.color = curr;
+
+    }
+}
