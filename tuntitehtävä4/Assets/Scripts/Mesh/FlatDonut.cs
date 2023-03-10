a6509fdd007a7cfa0126139770cd9a4fb4c172e4
nonparelli <90031028+nonparelli@users.noreply.github.com>
2023-03-10 15:51:50 +0200

    Kotitehtävä lol

--- tuntitehtävä4/Assets/Scripts/Mesh/FlatDonut.cs
+++ tuntitehtävä4/Assets/Scripts/Mesh/FlatDonut.cs
@@ -2,65 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleMesh : MonoBehaviour
+public class FlatDonut : MonoBehaviour
 {
     [Range(3, 255)]
     public int N = 8;
+    [Range(1,5)]
     public float Radius = 1.0f;
+    [Range(2,10)]
+    public float OuterRadius = 2.0f;
 
     private float TAU = 2 * Mathf.PI;
 
     private void GenerateMesh()
     {
         Mesh mesh = new Mesh();
-        // Circular Mesh
 
         List<Vector3> verts = new List<Vector3>();
 
-        verts.Add(Vector3.zero); // Add the center point of the "circle"
-        //Vector3 v = Vector3.up * Radius;
-        //verts.Add(v); // Add the first (zeroeth) vertex, which is just upwards
         for (int i = 0; i < N; i++)
         {
             float theta = TAU * i / N; // angle of current iteration
             Debug.Log("Angle: " + theta + ", which in deg is: " + 360f * theta / TAU);
             Vector3 v = new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0);
             verts.Add(v * Radius);
+            verts.Add(v * OuterRadius);
         }
-
         mesh.SetVertices(verts);
 
         List<int> tri_indices = new List<int>();
-        for (int i = 1; i < N; i++)
+        for (int i = 0; i < N - 1; i++)
         {
-            tri_indices.Add(0);
-            tri_indices.Add(i);
-            tri_indices.Add(i + 1);
+            int InnerFirst = 2 * i;
+            int OuterFirst = InnerFirst + 1;
+            int InnerSecond = OuterFirst + 1;
+            int OuterSecond = InnerSecond + 1;
+            //first tri
+            tri_indices.Add(InnerFirst);
+            tri_indices.Add(OuterFirst);
+            tri_indices.Add(OuterSecond);
+            // second tri
+            tri_indices.Add(InnerFirst);
+            tri_indices.Add(OuterSecond);
+            tri_indices.Add(InnerSecond);
         }
-        tri_indices.Add(0);
-        tri_indices.Add(N);
-        tri_indices.Add(1);
+        int InFirst = 2 * (N - 1);
+        int OutFirst = InFirst + 1;
+        int InSecond = 0;
+        int OutSecond = 1;
+
+        tri_indices.Add(InFirst);
+        tri_indices.Add(OutFirst);
+        tri_indices.Add(OutSecond);
+
+        tri_indices.Add(InFirst);
+        tri_indices.Add(OutSecond);
+        tri_indices.Add(InSecond);
+
         mesh.SetTriangles(tri_indices, 0);
 
         mesh.RecalculateNormals();
 
         GetComponent<MeshFilter>().sharedMesh = mesh;
-        /*
-        Vector3[] verts = {
-        new Vector3 (-1f, 1f, 0f),
-        new Vector3(1f, 1f, 0f),
-        new Vector3(-1f, -1f, 0f),
-        new Vector3(1f, -1f, 0f),
-        };
-        int[] tri_indices = {
-        2, 1, 0,
-        2, 3, 1
-        };
-        mesh.vertices = verts;
-        mesh.SetTriangles(tri_indices, 0);
-        mesh.RecalculateNormals();
-        GetComponent<MeshFilter>().sharedMesh = mesh;
-        */
     }
     void Start()
     {
